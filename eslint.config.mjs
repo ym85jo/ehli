5f6133ba9df9f8d1830f65dba06ba5e5aaa4cd70
Jo <ehil@nate.com>
2025-06-16 14:29:22 +0900

    '1st'

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      // 여기에 추가적인 규칙을 설정할 수 있습니다
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+    },
+  },
+  eslintConfigPrettier,
+];
+
+export default eslintConfig;
